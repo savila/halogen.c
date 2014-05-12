dbe8d43f72e811b69ec01fbd1530c4cfc0a9e9b8
savila <santiago.avila@uam.es>
2014-05-13 06:07:39 +0800

    v0.5 initiated: towards lognormal implemtentations

---
+++ v0.5/src/place_halos.h
@@ -0,0 +1,2 @@
+int place_halos(long, float *, long, long , float *, float *, float *, float , float , long,float,double *, double *,long,float *, float *, float *, float *);
+int place_halos_byparts(long, long , float *, long, long , float *, float *, float *, float , float , long,float,double *, double *,long,float *, float *, float *, float *,double *);
