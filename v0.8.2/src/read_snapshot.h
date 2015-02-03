8debfe68796b14e00254acce97566f3d7ed5d61e
sjap <savila@goliat.galilea>
2015-02-03 17:03:20 +0100

    2LPT-IC and Makefiles working

---
+++ v0.8.2/src/read_snapshot.h
@@ -0,0 +1,2 @@
+int read_snapshot(char *, int, float, float, int, int, int, int,int,float **, float **, float **, float **,float**,float**,long *, float *, float *, float *, long ***,long **,float **);
+int read_snapshot_serial(char *, int , float , float , int , int, int , int , int , float **, float **, float **, float **, float **, float **,long *, float *, float *, float*,long ***,long **,float **);
